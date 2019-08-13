28c9e78f3655b8bd206b61316cfa56e8ba8d8037
nihal-art <53474961+nihal-art@users.noreply.github.com>
2019-08-13 16:26:06 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,173 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>SamKGo | Resume</title>
+<link rel="shortcut icon" href="../../../images/favicon_sk.ico">
+<link href="css/style_resume.css" rel="stylesheet" type="text/css">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+<script src="https://use.typekit.net/xgu2cay.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+</head>
+
+<body>
+<header class="background-primary">
+  <div class="row">
+    <div class="col-1-3"><img src="http://www.samkgo.com/training/edX/css-introduction/images/photo_sk.png" alt="profile image"/> </div>
+    <div class="col-1-3">
+      <h2>SamKGo</h2>
+      <h1>Sarah Kahnert</h1>
+      <h3>Biologist | Graphic Designer</h3>
+    </div>
+    <div class="col-1-3"><img src="http://www.samkgo.com/training/edX/css-introduction/images/logo_sk_header.png" alt="logo"/> </div>
+  </div>
+  <div class="row">
+    <ul class="contact">
+      <li><i class="fa fa-map-marker"></i>&nbsp; Heidelberg, Germany</li>
+      <li><i class="fa fa-phone"></i>&nbsp; +49 123 1234567</li>
+      <li><i class="fa fa-envelope"></i>&nbsp; samkgo@xxx.com</li>
+    </ul>
+  </div>
+</header>
+<nav>
+  <ul>
+    <li><a href="#link1"><i class="fa fa-briefcase" aria-hidden="true"></i> Work Experience</a></li>
+    <li><a href="#link2"><i class="fa fa-graduation-cap" aria-hidden="true"></i> Education</a></li>
+    <li><a href="#link3"><i class="fa fa-certificate" aria-hidden="true"></i> Advanced Training</a></li>
+    <li><a href="#link4"><i class="fa fa-lightbulb-o" aria-hidden="true"></i> Skills</a></li>
+    <li><a href="#link5"><i class="fa fa-globe" aria-hidden="true"></i> Connect</a></li>
+  </ul>
+</nav>
+<main>
+  <section id="section1" class="background-tertiary">
+    <div class="row">
+      <div class="col-1-9"></div>
+      <div class="col-1-3">
+        <h1 id="link1"><i class="fa fa-briefcase" aria-hidden="true"></i> Work Experience</h1>
+        <article id="article1" class="background-secondary">
+          <p class="date">January 2009–Present</p>
+          <h3>Research Assistant | Science Communication</h3>
+          <h4>German Cancer Research Center, Unit Cancer Prevention</h4>
+        </article>
+        <article id="article2" class="background-secondary">
+          <p class="date">Before January 2009</p>
+          <h3>Freelancer in market research and employee in gastronomy sector</h3>
+          <h4>Gesellschaft für Innovative Marktforschung, Coyote Café, Hard Rock Café and others</h4>
+        </article>
+      </div>
+      <div id="timeline" class="col-1-9">
+        <h1><i class="fa fa-calendar" aria-hidden="true"></i></h1>
+        <ul>
+          <li>
+            <mark>2016</mark>
+          </li>
+          <li>2015</li>
+          <li>2014</li>
+          <li>
+            <mark>2013</mark>
+          </li>
+          <li>2012</li>
+          <li>2011</li>
+          <li>
+            <mark>2010</mark>
+          </li>
+          <li>
+            <mark>2009</mark>
+          </li>
+          <li>
+            <mark>2008</mark>
+          </li>
+          <li>2007</li>
+          <li>2006</li>
+          <li>2005</li>
+          <li>2004</li>
+          <li>2003</li>
+          <li>2002</li>
+          <li>
+            <mark>2001</mark>
+          </li>
+        </ul>
+      </div>
+      <div class="col-1-3">
+        <h1 id="link2"><i class="fa fa-graduation-cap" aria-hidden="true"></i> Education</h1>
+        <article id="article3" class="background-secondary">
+          <p class="date">December 2010–July 2013</p>
+          <h3>Certified Graphic Designer</h3>
+          <h4>Correspondence course at Fernakademie für Erwachsenenbildung, Hamburg, Germany</h4>
+        </article>
+        <article id="article4" class="background-secondary">
+          <p class="date">October 2001–September 2008</p>
+          <h3>Graduate Biologist | Dipl.-Biol.</h3>
+          <h4>Studies at Heidelberg University and Goethe University Frankfurt, Germany</h4>
+        </article>
+      </div>
+      <div class="col-1-9"></div>
+    </div>
+  </section>
+  <section id="section2" class="background-tertiary">
+   <div class="row">
+    <h1 id="link3"><i class="fa fa-certificate" aria-hidden="true"></i> Advanced Training</h1>
+    <article id="article5" class="background-secondary">
+      <p>Selection of attendance-based and online courses (one-day to several-week)</p>
+      <p><span>November–December 2016</span> | <b>Microsoft | DEV218x: Advanced CSS Concepts</b> | edX <span class="sep">//</span><span> November–December 2016</span> | <b>W3Cx | CSS.0x: CSS Introduction</b> | edX <span class="sep">//</span><span> November 2016</span> | <b>Microsoft Excel/Access</b> | GFN, Heidelberg, Germany <span class="sep">//</span><span> June–July 2016</span> | <b>W3Cx | HTML5 Part 2: Advanced Techniques for Designing HTML5 Apps</b> | edX <span class="sep">//</span><span> May–June 2016</span> | <b>W3Cx | HTML5 Part 1: HTML5 Coding Essentials and Best Practices</b> | edX <span class="sep">//</span><span> April–May 2016</span> | <b>W3Cx | HTML5.0x: HTML5 Introduction</b> | edX <span class="sep">//</span><span> April 2015</span> | <b>Publishing for Digital Media</b> | Cleverprinting, Frankfurt on the Main, Germany <span class="sep">//</span><span> October 2014</span> | <b>Office Productivity Workshop</b> | Cancom, Stuttgart, Germany <span class="sep">//</span><span> April 2013</span> | <b>Typography for Graphic and Communication Designers</b> | Wolfgang Beinert, Berlin, Germany <span class="sep">//</span><span> February 2013</span> | <b>Online Communication</b> | National Institute of Science Communication, Karlsruhe, Germany <span class="sep">//</span><span> January 2013</span> | <b>Inner Success</b> | Calvin Hollywood Photography, Oftersheim, Germany <span class="sep">//</span><span> October 2012</span> | <b>Color Management</b> | College of Higher Education, Frankfurt on the Main, Germany <span class="sep">//</span><span> October 2012</span> | <b>Acrobat – PDF as Prepress</b> | College of Higher Education, Frankfurt on the Main, Germany <span class="sep">//</span><span> August 2012</span> | <b>Website Programming with HTML</b> | College of Higher Education, Heidelberg, Germany <span class="sep">//</span><span> April–July 2012</span> | <b>Basic Principles in Biostatistics</b> | German Cancer Research Center, Heidelberg, Germany <span class="sep">//</span><span> December 2011</span> | <b>XAMPP with MySQL and PHP</b> | College of Higher Education, Heidelberg, Germany <span class="sep">//</span><span> September 2010</span> | <b>Writing Workshop for Scientists</b> | Klaus Tschira Foundation, Heidelberg, Germany <span class="sep">//</span><span> June 2010</span> | <b>Poster Design – Techniques</b> | German Cancer Research Center, Heidelberg, Germany</p>
+    </article>
+	  </div>
+  </section>
+  <section id="section3" class="background-tertiary">
+    <h1 id="link4"><i class="fa fa-lightbulb-o" aria-hidden="true"></i> Skills</h1>
+    <div class="row">
+      <div class="col-2-3">
+        <article id="article6" class="background-secondary">
+          <h2><i class="fa fa-bar-chart" aria-hidden="true"></i> analytical | creative <i class="fa fa-paint-brush" aria-hidden="true"></i> &nbsp;&nbsp;&nbsp;</h2>
+          <div id="tag-cloud">
+            <div id="cloud-left">
+              <p><span id="tag1">research</span><br>
+                <span id="tag2">study evaluation</span>&nbsp;&nbsp;&nbsp; <span id="tag3">scientific writing</span> <span id="tag4">statistical analysis</span>&nbsp;&nbsp;&nbsp; <span id="tag5">editing</span></p>
+            </div>
+            <div id="cloud-right">
+              <p><span id="tag6">illustration</span>&nbsp;&nbsp;&nbsp; <br>
+                <span id="tag7">print</span>&nbsp;&nbsp;&nbsp; <span id="tag8">drawing</span>&nbsp;&nbsp;&nbsp; <br>
+                <span id="tag9">digital art</span>&nbsp;&nbsp;&nbsp; 
+                <span id="tag10">photography</span><br><span id="tag11">web design</span></p>
+            </div>
+          </div>
+        </article>
+      </div>
+      <div class="col-1-3">
+        <article id="article7" class="background-secondary">
+          <h2><i class="fa fa-desktop" aria-hidden="true"></i> <i class="fa fa-wrench" aria-hidden="true"></i> tools</h2>
+          <div id="tools">
+            <ul>
+              <li>InDesign</li>
+              <li>Illustrator</li>
+              <li>Photoshop</li>
+              <li>Dreamweaver</li>
+              <li>Office</li>
+            </ul>
+            <ul class="circles">
+              <li><i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle-o" aria-hidden="true"></i></li>
+              <li><i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle-o" aria-hidden="true"></i> <i class="fa fa-circle-o" aria-hidden="true"></i></li>
+              <li><i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle-o" aria-hidden="true"></i> <i class="fa fa-circle-o" aria-hidden="true"></i></li>
+              <li><i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle-o" aria-hidden="true"></i> <i class="fa fa-circle-o" aria-hidden="true"></i> <i class="fa fa-circle-o" aria-hidden="true"></i> <i class="fa fa-circle-o" aria-hidden="true"></i></li>
+              <li><i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle" aria-hidden="true"></i> <i class="fa fa-circle-o" aria-hidden="true"></i></li>
+            </ul>
+          </div>
+        </article>
+      </div>
+    </div>
+  </section>
+</main>
+<footer class="background-primary"> <img src="http://www.samkgo.com/training/edX/css-introduction/images/logo_sk_footer.png" width="100" height="100" alt="logo"/>
+  <div>
+    <h1 id="link5"><i class="fa fa-globe" aria-hidden="true"></i> Connect | Check me out</h1>
+    <div id="connect">
+      <h2><a href="https://de.linkedin.com/in/sarah-kahnert/en" title="LinkedIn" target="_blank"><i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a></h2>
+      <h2><a href="https://www.behance.net/samgokahnert" title="Behance" target="_blank"><i class="fa fa-behance-square fa-2x" aria-hidden="true"></i></a></h2>
+      <h2><a href="https://twitter.com/SamKGo" title="Twitter" target="_blank"><i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i></a></h2>
+      <h2><a href="https://www.instagram.com/samkgo/" title="Instagram" target="_blank"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a></h2>
+    </div>
+  </div>
+  <p id="copyright">© SamKGo</p>
+</footer>
+</body>
+</html>
